a477115ec8e6adcb52e9d2c56f1edbcd13ee4643
KauztoKuro <ikkikurogane70@gmail.com>
2021-11-20 22:52:56 +0630

    icon

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -2,7 +2,12 @@ import React from 'react'
 // import Typography from '@material-ui/core/Typography'
 // import Button from '@material-ui/core/Button'
 // import ButtonGroup from '@material-ui/core/ButtonGroup'
-import { Typography, Button, Container } from '@material-ui/core'
+
+import { Typography, Button, Container } from '@mui/material' 
+
+// import AcUnitOutlinedIcon from '@mui/icons-material/AcUnitOutlined';
+import SendIcon from '@mui/icons-material/Send';
+import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
 
 export default function Create() {
   return (
@@ -22,6 +27,15 @@ export default function Create() {
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae velit temporibus dicta, consequuntur perspiciatis molestias consectetur reiciendis a neque, provident qui quam quidem animi dolor quod repudiandae cumque ipsam? At!
       </Typography> */}
 
+      {/* <Button type="submit" color="primary" >Submit</Button>
+      <Button type="submit" color="secondary" variant="outlined" >Submit</Button>
+
+      <ButtonGroup color="secondary" variant="contained" >
+        <Button>One</Button>
+        <Button>Two</Button>
+        <Button>Three</Button>
+      </ButtonGroup> */}
+
       <Typography 
         variant="h6"
         color="textSecondary"
@@ -36,19 +50,22 @@ export default function Create() {
         type="submit"
         color="secondary"
         variant="contained"
+        // startIcon={<SendIcon />}
+        endIcon={<KeyboardArrowRightIcon />}
         // disableElevation //which means drop shadow
       >
         Submit
       </Button>
 
-      {/* <Button type="submit" color="primary" >Submit</Button>
-      <Button type="submit" color="secondary" variant="outlined" >Submit</Button>
+      {/* Icons */}
+      {/* <br />
+      <AcUnitOutlinedIcon />
+      <AcUnitOutlinedIcon color="secondary" fontSize="large" />
+      <AcUnitOutlinedIcon color="secondary" fontSize="small" />
+      <AcUnitOutlinedIcon color="action" fontSize="small" />
+      <AcUnitOutlinedIcon color="error" fontSize="small" />
+      <AcUnitOutlinedIcon color="disabled" fontSize="small" /> */}
 
-      <ButtonGroup color="secondary" variant="contained" >
-        <Button>One</Button>
-        <Button>Two</Button>
-        <Button>Three</Button>
-      </ButtonGroup> */}
     </Container>
   )
 }
