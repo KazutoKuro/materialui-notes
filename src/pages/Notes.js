d154410c3e3ea677f8404227cb727522d6ceddf9
KauztoKuro <ikkikurogane70@gmail.com>
2021-11-21 23:16:27 +0630

    L-10: Grid System

--- src/pages/Notes.js
+++ src/pages/Notes.js
@@ -1,4 +1,7 @@
 import React, { useEffect, useState } from 'react'
+import { Grid } from '@mui/material'
+import { Paper } from '@mui/material'
+import { Container } from '@mui/material'
 
 export default function Notes() {
   const [notes, setNotes] = useState([])
@@ -9,10 +12,29 @@ export default function Notes() {
   }, [])
 
   return (
-    <div>
-      {notes.map(note => (
-        <p key={note.id}>{ note.title }</p>
-      ))}
-    </div>
+    <Container>
+      {/* <Grid container>
+        <Grid item xs={12} sm={6} md={3}>
+          <Paper>1</Paper>
+        </Grid>
+        <Grid item xs={12} sm={6} md={3}>
+          <Paper>2</Paper>
+        </Grid>
+        <Grid item xs={12} sm={6} md={3}>
+          <Paper>3</Paper>
+        </Grid>
+        <Grid item xs={12} sm={6} md={3}>
+          <Paper>4</Paper>
+        </Grid>
+      </Grid> */}
+
+      <Grid container>
+        {notes.map(note => (
+          <Grid item key={note.id} xs={12} md={6} lg={4}>
+            <Paper>{ note.title }</Paper>
+          </Grid>
+        ))}
+      </Grid>
+    </Container>
   )
 }
